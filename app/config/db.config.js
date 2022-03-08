72dbaf700ed5026eced3b8daf5acd3f8b00d5d5f
storres20 <sanchotorres777@gmail.com>
2022-03-08 16:57:27 -0500

    fix

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,14 @@
-module.exports = {
+/* module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
   DB: "products"
-};
+}; */
 
 //clever cloud
-/* module.exports = {
+module.exports = {
   HOST: "beadirub92exfstjx9on-mysql.services.clever-cloud.com",
   USER: "urj5wc9vcrnkygvg",
   PASSWORD: "urj5wc9vcrnkygvg",
   DB: "beadirub92exfstjx9on"
-}; */
+};
