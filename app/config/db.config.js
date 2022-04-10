064e3b3094998b28e6fa6bd6d5f1741d68ac7ff9
storres20 <sanchotorres777@gmail.com>
2022-04-10 02:27:18 -0500

    testing with docker

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,15 @@
-/* module.exports = {
+// testing with docker
+module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "",
+  PASSWORD: "123456",
   DB: "products"
-}; */
+};
 
 //clever cloud
-module.exports = {
+/* module.exports = {
   HOST: "beadirub92exfstjx9on-mysql.services.clever-cloud.com",
   USER: "urj5wc9vcrnkygvg",
   PASSWORD: "FWzZwlCU4XhjcnBpO4Mi",
   DB: "beadirub92exfstjx9on"
-};
+}; */
