ae92485945ff9af9edc48f0a229c66909456baa2
storres20 <sanchotorres777@gmail.com>
2022-04-23 01:21:57 -0500

    aws rds mariadb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -7,9 +7,17 @@
 }; */
 
 //clever cloud
-module.exports = {
+/* module.exports = {
   HOST: "beadirub92exfstjx9on-mysql.services.clever-cloud.com",
   USER: "urj5wc9vcrnkygvg",
   PASSWORD: "FWzZwlCU4XhjcnBpO4Mi",
   DB: "beadirub92exfstjx9on"
+}; */
+
+//AWS RDS mariaDB
+module.exports = {
+  HOST: "database-1.crwxddq8cva0.us-east-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "mariadb2022",
+  DB: "realplaza"
 };
